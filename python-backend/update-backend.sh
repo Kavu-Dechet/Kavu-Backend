7fedd3bc010934d787cce98aeb1f6c0e6aa800af
ymougenel <yann.mougenel@gmail.com>
2023-01-11 19:33:58 +0300

    fix[script]: fix docker deployment script

--- python-backend/update-backend.sh
+++ python-backend/update-backend.sh
@@ -1,4 +1,3 @@
-git pull
-docker-compose stop kavu-backend
-docker-compose build kavu-backend
-docker-compose start kavu-backend
+docker-compose stop kavu-backend;
+docker-compose build kavu-backend;
+docker-compose up kavu-backend;
